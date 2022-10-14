62eab3ec50556c0355ea3431824a0f4db8040f97
Agustin Ricardo <agustinricardo14@gmail.com>
2022-10-14 10:25:05 -0300

    update turn

--- src/controllers/turn.controller.js
+++ src/controllers/turn.controller.js
@@ -1,21 +1,13 @@
-const { validationResult } = require('express-validator');
 const pool = require('../db');
 
 const newTurn = async (req, res, next) => {
 
-  const errors = validationResult( req );
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      errors: errors.array()
-    });
-  }
 
 try {
   const { user_id, prof_id, prepaid_id, place_id, payment_id, hour, date, treatment } = req.body
   
   const result = await pool.query(`INSERT INTO "Turn" (user_id, prof_id, prepaid_id, place_id, payment_id, hour, date, treatment)
-                                       VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`
+                                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
                                       ,[
                                         user_id, 
                                         prof_id, 
@@ -31,7 +23,6 @@ try {
     success: true,
     message: "Se agendó tu turno correctamente"
     });
-    res.send('Se agendó tu turno correctamente')
 } catch (error) {
   next(error)
   console.log(error.message)
