cfaede786d1715168e329cfa10f3a07665c13e2a
Agustin <agustinricardo14@gmail.com>
2023-06-19 18:30:15 -0300

    correción bug turno

--- src/controllers/turn.controller.js
+++ src/controllers/turn.controller.js
@@ -4,10 +4,10 @@ const newTurn = async (req, res, next) => {
 
 
 try {
-  const { user_id, prof_id, prepaid_id, place_id, payment_id, hour, date, treatment } = req.body
+  const { user_id, prof_id, prepaid_id, place_id, hour, date, treatment } = req.body
   
   const result = await pool.query(`INSERT INTO "Turn" (user_id, prof_id, prepaid_id, place_id, hour, date, treatment)
-                                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
+                                        VALUES ($1, $2, $3, $4, $5, $6, $7)`
                                       ,[
                                         user_id, 
                                         prof_id, 
