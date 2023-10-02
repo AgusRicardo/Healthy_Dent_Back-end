b0192b5373f2e3c0a34b87256c2d6c049649f3c9
Agustin Ricardo <agustinricardo14@gmail.com>
2023-10-02 18:38:46 -0300

    Incorporacion tabla especialidades

--- src/controllers/professionalList.controller.js
+++ src/controllers/professionalList.controller.js
@@ -2,13 +2,16 @@ const pool = require('../db');
 
 const getAllProfessional = async (req, res, next) => {
   try {
-    const allProfessional = await pool.query(`SELECT
-                                                CONCAT(name, ' ', last_name) AS name
-                                                ,p.prof_id
-                                                ,n_matric
-                                                ,specialization
-                                              FROM "User" as u
-                                              INNER JOIN "Professional" as p
+    const allProfessional = await pool.query(`SELECT 
+                                              CONCAT(name, ' ', last_name) AS name
+                                              ,p.prof_id
+                                              ,p.n_matric
+                                              ,s.description 
+                                              ,s.spe_id
+                                            from "Professional" p 
+                                            inner join "Specialization" s 
+                                              on p.specialization = s.specialization
+                                            inner join "User" u 
                                               ON u.user_id = p.user_id;
                                               `)
     res.json(allProfessional.rows)
