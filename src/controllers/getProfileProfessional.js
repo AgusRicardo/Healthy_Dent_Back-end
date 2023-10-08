e2fb2422ee025e0a6278dd6874b15920cb4d486f
Agustin Ricardo <agustinricardo14@gmail.com>
2023-10-08 16:09:44 -0300

    Correci''on bug perfil profesional

--- src/controllers/getProfileProfessional.js
+++ src/controllers/getProfileProfessional.js
@@ -6,11 +6,21 @@ try {
   const { id } = req.params
   
   const result = await pool.query(`
-                                    select u.user_id, u.name, u.last_name, u.date_birth,u.telephone,u.address_user ,u.email_user, p.n_matric, p.specialization  
-                                    from "User" as u 
-                                    inner join "Professional" as p 
-                                    on u.user_id = p.user_id
-                                    where u.user_id = $1`,[id])
+  select u.user_id
+	,u.name
+	,u.last_name
+	,u.date_birth
+	,u.telephone
+	,u.address_user 
+	,u.email_user
+	,p.n_matric
+	,s.description as specialization
+from "User" as u 
+inner join "Professional" as p 
+	on u.user_id = p.user_id
+inner join "Specialization" s 
+	on p.specialization = s.specialization 
+where u.user_id = $1`,[id])
   res.json(result.rows);
 } catch (error) {
   next(error)
