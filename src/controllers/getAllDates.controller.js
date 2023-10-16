5cd0676d6d7413d06a82153d9fb1dbaebf5a4f42
Gisela <giselladieguez2@gmail.com>
2023-10-16 20:21:32 -0300

    #563 - Actualizo query para no mostrar fechas viej

--- src/controllers/getAllDates.controller.js
+++ src/controllers/getAllDates.controller.js
@@ -6,12 +6,13 @@ try {
   const { id } = req.params
   
   const result = await pool.query(`SELECT distinct(date)
-	,prof_id
+	, prof_id
 	, place_id
 	, availability
 	FROM "Turn"
 WHERE prof_id = $1
 	AND (availability = true OR availability IS NULL)
+  AND date >= CURRENT_DATE
 ORDER BY date ASC;`,[id])
   if(result.rows.length === 0){
     return res.json({
